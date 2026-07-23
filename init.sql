7014442e2471348767e1b64d694d52402176615d
Zaza1892 <90157965+Zaza1892@users.noreply.github.com>
2026-07-23 11:47:25 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -16,7 +16,7 @@ CREATE TABLE device_lookups (
     -- this creates a table named "device_lookups"
     id SERIAL PRIMARY KEY,
     --auto-incrementing unique id for each row
-    message_id TEXT NOT NULL,
+    message_id TEXT NOT NULL UNIQUE,
     --the original message_id from the RabbitMQ payload
     device_id TEXT NOT NULL,
     --the device_id  extracted
