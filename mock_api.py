d6cabbc1f50c5d63be064f98244c6477b1741a01
Zaza1892 <90157965+Zaza1892@users.noreply.github.com>
2026-08-02 19:23:00 +0200

    Update mock_api.py

--- mock_api.py
+++ mock_api.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI  
 from pydantic import BaseModel  
 import random 
-import hashlib # used to turn a device id into a repeatable number, so the same device always gets the same tenant 
+import hashlib 
 
-############################################################
 
 app= FastAPI()  
 @app.get("/health")
@@ -20,7 +19,6 @@ def health():
 class DeviceRequest(BaseModel): 
     device_id:str
 
-############################################################
 
 @app.post("/lookup")  
 def lookup_tenants(request: DeviceRequest):
@@ -36,8 +34,7 @@ def lookup_tenants(request: DeviceRequest):
 
 
     device_seed = int(hashlib.sha256(request.device_id.encode()).hexdigest(),16)
-    #^ seed the random generator using the device id , so the device id always produces the same result
-    random_generator = random.Random(device_seed)
+    random_generator = random.Random(  device_seed)
 
     num_tenants = random_generator.randint(1,3)  
     tenant_id = [f"tenant-{random_generator.randint(1000,9999)}" for _ in range(num_tenants)] 
@@ -49,4 +46,3 @@ def lookup_tenants(request: DeviceRequest):
         "tenants":tenant_id 
                 }
 
-############################################################
